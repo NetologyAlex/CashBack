565676b777dc994fae821ea2cb7c17bec606118a
Alex Nekrasov <alexandrnekrasov1988@gmail.com>
2022-08-11 14:58:50 +0300

    commit

---
+++ src/test/CashBackHackerTest.java
@@ -0,0 +1,24 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackerServiceTest {
+
+}
+
+    @Test
+    public void shouldCalculateFrom900() {
+        CashbackHackerService service = new CashbackHackerService();
+        Assert.assertEquals(service.remain(900), 100);
+    }
+
+    @Test
+    public void shouldCalculateFrom0() {
+        CashbackHackerService service = new CashbackHackerService();
+        Assert.assertEquals(service.remain(0), 1000);
+    }
+
+
+}
